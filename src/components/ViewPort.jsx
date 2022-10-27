ac498245042d969c14cff9cccead9c8d1f357d39
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2022-10-27 12:26:15 -0700

    Fit images into the slide

--- src/components/ViewPort.jsx
+++ src/components/ViewPort.jsx
@@ -1,20 +1,28 @@
 
 import React, { useState, useEffect } from 'react';
-import * as cornerstone from 'cornerstone-core'
-import { getExampleImage } from '../utils/imageGenerator'
+import * as cornerstone from 'cornerstone-core';
+import { getExampleImage } from '../utils/imageGenerator';
+import './style.css';
 
 const ViewPort = ({ imageIds, currentSlide }) => {
     useEffect(() => {
+        const box = document.getElementById('viewPort-wrapper');
+        const domRect = box.getBoundingClientRect();
+        const wrapperHeight = domRect.height;
+        const height = imageIds.length ? wrapperHeight / imageIds.length : domRect.height;
         imageIds.forEach((id, i) => {
             const element = document.getElementById(`dicomImage${currentSlide}${i}`);
             cornerstone.enable(element);
             const imageData = getExampleImage(id).promise;
             imageData.then(function (image) {
                 cornerstone.displayImage(element, image);
-
+                element.style.height = height + 'px';
+                cornerstone.resize(element);
             });
         });
-    }, [imageIds])
+    }, [imageIds, currentSlide])
+
+
 
     return (
         <div id="viewPort-wrapper">
